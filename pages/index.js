773c55f47879f88096a2fb1428cb859b8ee6be22
https://github.com/AidenTran15 <aidenkiettran@gmail.com>
2022-12-25 16:43:23 +1100

    added Banner component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 // import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Banner from '../components/banner'
 
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Coffeeee Connoisseur</h1>
+        <Banner/>
       </main>
 
     </div>
