4791f94b4ff445bc8ed365f02ef220eedafc4c9d
AidenTran15 <aidenkiettran@gmail.com>
2023-01-09 17:53:35 +1100

    applied style for card component

--- components/card.js
+++ components/card.js
@@ -1,11 +1,14 @@
+import Link from "next/link";
+import Image from "next/image"
+
+import styles from './card.module.css'
 const Card = (props) => {
     return (
         <Link href={props.href}>
-        <a>
-            <h2>
+            
+                <h2>{props.name}</h2>
                 <Image src={props.imgUrl} width={260} height={160}/>
-            </h2>
-        </a>
+            
         </Link>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,9 @@ export default function Home() {
           <div className ={styles.heroImage}>
             <Image src="/static/hero-image.png" width={700} height={400}/>
             </div>
-        <Card/>
+        <Card name="DarkHorse Coffee" 
+              imgUrl="/static/hero-image.png"
+              href="/coffee-store/darkhorse-coffee"/>
       </main>
 
     </div>
